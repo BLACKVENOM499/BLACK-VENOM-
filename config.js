3f2d0ca40dde9f22891a68981c5efd81b659dfce
BLACKVENOM499 <blackvenomx499@gmail.com>
2024-12-28 08:59:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID, || "",
 ALIVE_IMG: process.env.ALLIVE_IMG || "enter url",
 ALIVE_MSG: process.env.ALIVE_MSG || "ʜɪ ᴛʜɪꜱ ɪꜱ ʙʟᴀᴄᴋ ᴠᴇɴᴏᴍ",
 };
