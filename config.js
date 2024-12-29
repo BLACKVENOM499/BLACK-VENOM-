9693a13c38d75dd6d1f231eec68fea4c546cb746
BLACKVENOM499 <blackvenomx499@gmail.com>
2024-12-29 11:18:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,1 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
-module.exports = {
-SESSION_ID: process.env.SESSION_ID, || "",
-ALIVE_IMG: process.env.ALLIVE_IMG || "enter url",
-ALIVE_MSG: process.env.ALIVE_MSG || "ʜɪ ᴛʜɪꜱ ɪꜱ ʙʟᴀᴄᴋ ᴠᴇɴᴏᴍ",
-};
